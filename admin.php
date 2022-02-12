2f54b9086b5b81e2780173234df1dc9c0b5f9f91
Daniel <danielcarloventura@gmail.com>
2022-02-12 15:00:31 +0800

    starter kits
    starter kits need to be moved

---
+++ admin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin page</title>
+
+    <!-- font awesome cdn here-->
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/">
+
+    <!-- custom css-->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<div class="container">
+    <section>
+
+    <form action="" class="add-product-form" method="post" enctype="multipart/form-data">
+        <h3>add a new product</h3>
+        <input type="text" name="p_name" placeholder="enter the product nmae" class="box" required>
+    </form>
+    </section>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom script-->
+<script src="js/script.js"></script>
+</body>
+</html>
